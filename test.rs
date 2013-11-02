0e245899a17c4c39d13c293432fe7e6cd6eca43b
KENZ <KENZ.gelsoft@gmail.com>
2013-11-03 04:15:09 +0900

    Use macro_rules to make wx_main calling wrapper.

--- test.rs
+++ test.rs
@@ -1,4 +1,5 @@
 #[feature(globs)];
+#[feature(macro_rules)];
 #[feature(managed_boxes)];
 
 extern mod wx;
@@ -15,18 +16,23 @@ use wx::wrapper::*;
 static nullptr: *mut c_void = 0 as *mut c_void;
 
 
-#[start]
-fn start(argc: int, argv: **u8) -> int {
-    start_on_main_thread(argc, argv, on_main)
-}
+macro_rules! wxApp(
+    ($f: ident) => (
+        fn start(argc: int, argv: **u8) -> int {
+            #[start];
+            fn on_main() {
+                #[fixed_stack_segment];
+                #[inline(never)];
+                let closure = wxClosure::new($f as *mut c_void, nullptr);
+                let args: ~[*i32] = ~[];
+                ELJApp::initializeC(closure, args.len() as i32, vec::raw::to_ptr(args) as *mut *mut i8);
+            }
+            start_on_main_thread(argc, argv, on_main)
+        }
+    )
+)
 
-#[fixed_stack_segment]
-#[inline(never)]
-fn on_main() {
-    let closure = wxClosure::new(wx_main as *mut c_void, nullptr);
-    let args: ~[*i32] = ~[];
-    ELJApp::initializeC(closure, args.len() as i32, vec::raw::to_ptr(args) as *mut *mut i8);
-}
+wxApp!(wx_main)
 
 extern "C"
 fn wx_main() {
