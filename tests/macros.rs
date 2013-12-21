06bd9b5a6edd79fbeac655c7fb3d741c02b2659c
KENZ <KENZ.gelsoft@gmail.com>
2013-12-21 21:00:10 +0900

    Try to fix build.

--- tests/macros.rs
+++ tests/macros.rs
@@ -19,7 +19,7 @@ pub macro_rules! wxApp(
                 #[inline(never)];
                 let closure = wxClosure::new($f as *mut c_void, nullptr);
                 let args: ~[*i32] = ~[];
-                ELJApp::initializeC(closure, args.len() as i32, vec::raw::to_ptr(args) as *mut *mut i8);
+                ELJApp::initializeC(closure, args.len() as i32, args.as_ptr() as *mut *mut i8);
             }
             start_on_main_thread(argc, argv, on_main)
         }
