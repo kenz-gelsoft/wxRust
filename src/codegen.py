30a91b7cbead4d13402183264d0eccd7bd057e22
KENZ <KENZ.gelsoft@gmail.com>
2014-01-05 13:51:29 +0900

    Don't remove `-DwxMAC` flag from the `wx-config --cppflags` output.

--- src/codegen.py
+++ src/codegen.py
@@ -990,7 +990,6 @@ class Preprocessor(object):
 
     def _call_cpp(self, file):
         cppflags = Popen(['wx-config', '--cppflags'], stdout=PIPE).communicate()[0].split()
-        cppflags.remove('-D__WXMAC__')
         cmdline = ['cpp', '-DWXC_TYPES_H'] + cppflags + ['-I/Users/kenz/src/wxRust/wxHaskell/wxc/src/include', file]
         return Popen(cmdline, stdout=PIPE).communicate()[0]
 
